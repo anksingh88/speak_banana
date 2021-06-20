11b3f1af0ebae1bf12fb35f4b6fd318afd045fdb
anksingh88 <88anksingh@gmail.com>
2021-06-20 20:24:57 +0530

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+// //input
+// var username= prompt("enter username")
+
+// //processing 
+// var welcomemessage = "this script works " +username;
+
+// //output
+// alert(welcomemessage)
+
+
+var btntranslate = document.querySelector("#btn-translate")
+//console.log(btntranslate);
+function clickHandler() {
+    console.log("clicked");
+}
+btntranslate.addEventListener("click", clickHandler) 
+    
